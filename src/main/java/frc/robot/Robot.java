e6a79b493e96a70802b7bd7a331e7c06452004a8
Rushali B <26rbetala@team102.org>
2025-12-17 20:10:42 -0500

    Made code for typing input, can't get widget to work- need to test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,24 +15,45 @@ import edu.wpi.first.networktables.Topic;
 import edu.wpi.first.wpilibj.*;
 
 import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilderImpl;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 
 
 public class Robot extends TimedRobot {
-    private SendableChooser<String> autoProgram;
-    private static final String Option1 = "Option1";
-    private static final String DoNothing = "Do Nothing";
-    private static final String Dancey = "Dance";
+    private SendableChooser<Float> autoProgram;
+    private static final float Option1 = 67;
+    private static final float Option2 = 41;
+    private static final float Option3 = 21;
+    private SendableBuilder inputData;
+
 
     public Robot() {
       autoProgram= new SendableChooser<>();
       autoProgram.setDefaultOption("Option 1", Option1);
-      autoProgram.addOption("Do Nothing", DoNothing);
-      autoProgram.addOption("Dance", Dancey);
+      autoProgram.addOption("Option2", Option2);
+      autoProgram.addOption("Option3", Option3);
       SmartDashboard.putData("Auto Program", autoProgram);
     }
+    
+    public class InputData implements Sendable {
+      private double targetValue =0.0; 
+
+
+      @Override
+      public void initSendable(SendableBuilder builder){
+        builder.setSmartDashboardType("InputData");
+        builder.addDoubleProperty("Target Setpoint",
+        () -> targetValue, //getter
+        (val) -> targetValue = val //setter 
+        );
+        
+        //SmartDashboard.putData("User Inputs",inputData);
+      }
+      }
     
 
 
@@ -44,4 +65,6 @@ public class Robot extends TimedRobot {
       System.out.println("Testing:" + autoProgram.getSelected());
     }
 
+     
+    
   }
